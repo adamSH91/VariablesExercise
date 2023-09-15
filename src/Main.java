1d6980de741d2bdd909f0f064a5ca29cc97bb7ed
adamSH91 <adamserraj91@gmail.com>
2023-09-15 11:47:00 +0100

    finished task 3 user input

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Task 1 - String variables
         /*
         String firstName = "John", lastName = "Jones", name = firstName + " " + lastName;
         System.out.println(name);
          */
 
+        // Task 2 - Numbers
         int num1 = 1, num2 = 2;
         System.out.println(num1 + num2);
 
@@ -13,5 +17,22 @@ public class Main {
         System.out.println(num3++);
         System.out.println(num3);
         System.out.println(++num3);
+
+        // Task 3 - User input
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter your first name:");
+        String firstName = scan.next();
+        System.out.println("Please enter your last name:");
+        String lastName = scan.next();
+        System.out.println("Hello " + firstName + " " + lastName);
+
+        System.out.println("Please enter a number:");
+        int firstNum = scan.nextInt();
+        System.out.println("Please enter another number:");
+        int secondNum = scan.nextInt();
+        int sum = firstNum + secondNum;
+        System.out.println("The sum of the numbers is " + sum);
+
     }
 }
