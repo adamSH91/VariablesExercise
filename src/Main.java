51afb16175d5650a7e34155c151a3e90150c930d
adamSH91 <adamserraj91@gmail.com>
2023-09-15 11:37:39 +0100

    finished task 2 numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        /*
         String firstName = "John", lastName = "Jones", name = firstName + " " + lastName;
         System.out.println(name);
+         */
+
+        int num1 = 1, num2 = 2;
+        System.out.println(num1 + num2);
+
+        double result = (double) num1 / num2;
+        int num3 = 8;
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3);
     }
 }
