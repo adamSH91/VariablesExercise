9a46526ebb6ea67a483e5c44bbf13db09a94c4d9
adamSH91 <adamserraj91@gmail.com>
2023-09-15 11:26:55 +0100

    finished task 1 string variables

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "John", lastName = "Jones", name = firstName + " " + lastName;
+        System.out.println(name);
+    }
+}
